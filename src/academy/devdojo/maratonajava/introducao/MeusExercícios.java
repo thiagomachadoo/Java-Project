246daf326c49d45389bd1847ebae1cb7253cc548
thiagomachadoo <thiagodasilvamachadoo44@gmail.com>
2021-07-19 11:00:11 -0300

    exercício com operadores primitivo sozinho

---
+++ src/academy/devdojo/maratonajava/introducao/MeusExercícios.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class MeusExercícios {
+    public static void main(String[] args){
+        int idade = 18;
+        String name = "Thiago Machado";
+        String consegui = "entrei na Generation, só boraaaaaaaaaa";
+        String dateFirstDay = "19/07/2021";
+        String generatiOn = ("Eu "+name+" tenho "+idade+" fui aprovado e "+consegui+" na data de "+dateFirstDay);
+        System.out.println(generatiOn);
+    }
+}
