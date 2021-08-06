abeb44038c190820c060e6dc4426428c6ec7c35f
thiagomachadoo <thiagodasilvamachadoo44@gmail.com>
2021-08-05 23:51:38 -0300

    teste

---
+++ src/academy/devdojo/maratonajava/introducao/teste.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class teste {
+    public static void main(String[] args) {
+        int idade = 18;
+        String name = "Thiago Machado";
+        String consegui = "entrei na Generation, só boraaaaaaaaaa";
+        String dateFirstDay = "19/07/2021";
+        String generatiOn = ("Eu "+name+" tenho "+idade+" fui aprovado e "+consegui+" na data de "+dateFirstDay);
+        System.out.println(generatiOn);
+    }
+}
